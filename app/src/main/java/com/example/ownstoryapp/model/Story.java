b1b81822436d0ba675e989b6e0a4dcfdfca864d9
Chris <CAlfano1999@gmail.com>
2020-05-12 18:07:09 -0400

    Latest

--- app/src/main/java/com/example/ownstoryapp/model/Story.java
+++ app/src/main/java/com/example/ownstoryapp/model/Story.java
@@ -1,5 +1,13 @@
 package com.example.ownstoryapp.model;
 
-public class Story {
+import com.example.ownstoryapp.R;
 
+public class Story {
+    private Page[] pages;
+    public Story(){
+        //Create an array of page objects, designates 7 spaces.
+        pages = new Page[7];
+
+        pages[0] =  new Page(R.drawable.page0, R.string.page0, new Choice(R.string.page0_choice1, 1), new Choice(R.string.page0_choice2, 2));
+    }
 }
